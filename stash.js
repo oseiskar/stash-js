7a8f7f463a93461c9d6b6b3d298d69a355cea481
Otto Seiskari <otto.seiskari@gmail.com>
2014-03-28 00:10:23 +0200

    watching location in front view

--- stash.js
+++ stash.js
@@ -160,13 +160,44 @@ function renderNewStash( coords ) {
     var previewBox = $('#result-box');
     previewBox.html('');
     
-    previewBox.append(buildButton( 'Refresh location', 'btn-default', function() {
+    var btnGroup = $('<div/>', {'class': 'btn-group btn-group-justified'} );
+    
+    var leftBtnGroup = $('<div/>', {'class': 'btn-group'} );
+    leftBtnGroup.append(buildButton( 'Refresh location', 'btn-default', function() {
         $('#preview-frame-box').html('');
         getLocation( function (loc) { renderNewStash( loc.coords ); } );
     }));
+    btnGroup.append(leftBtnGroup);
+    previewBox.append(btnGroup);
     
-    previewBox.append(buildEditableCoordinateBox(coords));
-    previewBox.append(buildGoogleMap( coords ));
+    var coordBox = $('<div/>');
+    var mapBox = $('<div/>');
+    coordBox.append(buildEditableCoordinateBox(coords, true));
+    mapBox.append(buildGoogleMap( coords ));
+    previewBox.append(coordBox);
+    previewBox.append(mapBox);
+    
+    var rightBtnGroup = $('<div/>', {'class': 'btn-group'} );
+    rightBtnGroup.append(buildButton( 'Watch location', 'btn-warning', function() {
+        $('#preview-frame-box').html('');
+        $('#stash-box').hide();
+        $('#stash-button').hide();
+        var watchId = watchLocation( function (loc) {
+            coordBox.html('');
+            coordBox.append(buildEditableCoordinateBox(loc.coords, false));
+        });
+        leftBtnGroup.html(buildButton( 'Show on map', 'btn-default', function() {
+            mapBox.html('');
+            mapBox.append(buildGoogleMap( coords ));
+            mapBox.append("fooasdfobar");
+        } ));
+        rightBtnGroup.html(buildButton( 'Stop watch', 'btn-success', function() {
+            stopWatching( watchId );
+            renderNewStash( window.lastWatchCoords );
+            $('#stash-box').show();
+        } ));
+    }));
+    btnGroup.append(rightBtnGroup);
     
     doStash(coords);
 }
@@ -187,7 +218,7 @@ function buildLinkButton( params, classes ) {
     },params));
 }
 
-function buildEditableCoordinateBox( coords ) {
+function buildEditableCoordinateBox( coords, editable ) {
 
     var coordinateBox = $('<div/>',  {
         'id': 'preview-coordinate-box',
@@ -199,44 +230,46 @@ function buildEditableCoordinateBox( coords ) {
     innerCoordinateBox.append($('<p/>', {text: 'Latitude: '+coords.latitude}));
     innerCoordinateBox.append($('<p/>', {text: 'Longitude: '+coords.longitude}));
     
-    var editButton = $('<input/>', {
-        type: 'button',
-        'class': 'btn btn-default btn-xs pull-right',
-        value: 'Edit' });
-        
-    editButton.click( function () {
-        var box = $('#preview-coordinate-box');
-        box.html('');
-        
-        var innerBox = $('<div/>', {
-            'class': 'panel-body',
-        });
-        
-        var buildInput = function ( id, label, value ) {
-            var formGroup = $('<div/>', {'class': 'form-group'});
-            formGroup.append($('<label/>', {text: label}));
-            formGroup.append($('<input/>', {
-                id: id,
-                type: 'text',
-                value: value,
-                'class': 'form-control'
+    if (editable) {
+        var editButton = $('<input/>', {
+            type: 'button',
+            'class': 'btn btn-default btn-xs pull-right',
+            value: 'Edit' });
+            
+        editButton.click( function () {
+            var box = $('#preview-coordinate-box');
+            box.html('');
+            
+            var innerBox = $('<div/>', {
+                'class': 'panel-body',
+            });
+            
+            var buildInput = function ( id, label, value ) {
+                var formGroup = $('<div/>', {'class': 'form-group'});
+                formGroup.append($('<label/>', {text: label}));
+                formGroup.append($('<input/>', {
+                    id: id,
+                    type: 'text',
+                    value: value,
+                    'class': 'form-control'
+                }));
+                return formGroup;
+            };
+            
+            innerBox.append( buildInput( 'lat-edit', 'Latitude', coords.latitude) );
+            innerBox.append( buildInput( 'long-edit', 'Longitude', coords.longitude) );
+            innerBox.append( buildButton( 'Stash these', 'btn-info', function() {
+                var la = $('#lat-edit').val();
+                var lo = $('#long-edit').val();
+                var newCoords = $.extend({}, coords, { latitude: la, longitude: lo });
+                renderNewStash( newCoords );
+                showStashBox();
             }));
-            return formGroup;
-        };
-        
-        innerBox.append( buildInput( 'lat-edit', 'Latitude', coords.latitude) );
-        innerBox.append( buildInput( 'long-edit', 'Longitude', coords.longitude) );
-        innerBox.append( buildButton( 'Stash these', 'btn-info', function() {
-            var la = $('#lat-edit').val();
-            var lo = $('#long-edit').val();
-            var newCoords = $.extend({}, coords, { latitude: la, longitude: lo });
-            renderNewStash( newCoords );
-            showStashBox();
-        }));
-        box.append(innerBox);
-    });
-        
-    coordinateBox.append( editButton );
+            box.append(innerBox);
+        });
+            
+        coordinateBox.append( editButton );
+    }
     coordinateBox.append(innerCoordinateBox);
 
     return coordinateBox;
@@ -290,12 +323,17 @@ commonGeoOptions = {
 function watchLocation( callback )
 {
     var opts = $.extend({}, commonGeoOptions, { maximumAge: 5000 });
-    window.watchId = navigator.geolocation.watchPosition( callback, showGeoError, opts );
+    window.watchId = navigator.geolocation.watchPosition( function ( loc ) {
+        window.lastWatchCoords = loc.coords;
+        callback(loc);
+    }, showGeoError, opts );
+    return window.watchId;
 }
 
 
-function stopWatching() {
-    navigator.geolocation.clearWatch(window.watchId);
+function stopWatching( watchId ) {
+    if (watchId === undefined) watchId = window.watchId;
+    navigator.geolocation.clearWatch(watchId);
 }
 
 function getLocation( callback )
