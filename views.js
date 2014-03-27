c116b36e9237c5161493e680d65849d58dd22685
Otto Seiskari <otto.seiskari@gmail.com>
2014-03-27 09:42:01 +0200

    distance-only mode

--- views.js
+++ views.js
@@ -29,31 +29,29 @@ window.stashViews = {
     }
   },
   
+  distonly: { 
+    name: "Distance only",
+    renderer: function ( elem, stashed, current ) {
+        elem.html('');
+        
+        var distance = getDistanceBetween( stashed, current );
+        console.log(distance);
+        distance = Math.round(distance).toString() + " m";
+        
+        elem.append($('<div/>', {
+            'class': 'command',
+            text: distance }));
+    }
+  },
+  
   cardinal: {
     name: "Cardinal direction",
     renderer: function ( elem, stashedCoords, currentCoords ) {
     
         elem.html('');
         
-        // won't work on strange places (on the 180 meridian)
-        var dlat = stashedCoords.latitude - currentCoords.latitude;
-        var dlong = stashedCoords.longitude - currentCoords.longitude;
-        
-        var adlat = Math.abs(dlat), adlong = Math.abs(dlong);
-        
-        dir = 'nowhere';
-        
-        if (adlat > adlong) {
-            // greater deviation in north-south vs west-east
-            if (dlat > 0) dir = 'north';
-            else dir = 'south';
-        }
-        else {
-            if (dlong > 0) dir = 'east';
-            else dir = 'west';
-        }
-        
-        command = "walk "+dir;
+        var dir = getCardinalDirection( currentCoords, stashedCoords );
+        var command = "walk "+dir;
         
         elem.append($('<div/>', {
             'class': 'command',
@@ -82,3 +80,69 @@ window.stashViews = {
 };
 
 window.defaultStashView = 'debug';
+
+
+// coordinate helpers
+function degToRad( deg ) {
+    return deg / 180.0 * Math.PI;
+}
+
+function vecMult( v, scalar ) {
+    return { x: v.x*scalar, y: v.y*scalar, z: v.z*scalar };
+}
+
+function vecPlus( a, b ) {
+    return { x: a.x + b.x, y: a.y + b.y, z: a.z + b.z }
+}
+
+function vecMinus( a, b ) { return vecPlus(a, vecMult(b, -1.0)); }
+function vecDot( a, b ) { return a.x*b.x + a.y*b.y + a.z*b.z; }
+function vecLength( v ) { return Math.sqrt(vecDot(v,v)); }
+function vecDirection( v ) { return vecMult( v, 1.0/vecLength( v ) ); }
+
+function getGlobalXYZ( coords ) {
+    lat = degToRad(coords.latitude);
+    long = degToRad(coords.longitude);
+    
+    var R = 6371000.0;
+    
+    return {
+        x: Math.sin(long)*Math.cos(lat) * R,
+        y: Math.cos(long)*Math.cos(lat) * R,
+        z: Math.sin(lat) * R };
+}
+
+function getDistanceBetween( a, b ) {
+    return vecLength( vecMinus( getGlobalXYZ(b), getGlobalXYZ(a) ) );
+}
+
+function getCardinalDirection( from, to ) {
+        // won't work on strange places (on the 180 meridian)
+        var dlat = to.latitude - from.latitude;
+        var dlong = to.longitude - from.longitude;
+        
+        var adlat = Math.abs(dlat), adlong = Math.abs(dlong);
+        
+        dir = 'nowhere';
+        
+        if (adlat > adlong) {
+            // greater deviation in north-south vs west-east
+            if (dlat > 0) dir = 'north';
+            else dir = 'south';
+        }
+        else {
+            if (dlong > 0) dir = 'east';
+            else dir = 'west';
+        }
+        return dir;
+}
+
+/*function getLocalCoordinateSystem( coords ) {
+    
+    var pos = getGlobalXYZ( coords );
+    var northPole = getGlobalXYZ( { latitude: 90, longitude: 0 } );
+    var toNorthPole = vecMinus( northPole, coords );
+    return {
+        north: vecDirection(toNorthPole) // TODO
+    };
+}*/
